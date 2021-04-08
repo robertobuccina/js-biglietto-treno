9989bbf57be35cde6377104b18f03099724c6163
robertobuccina <77169589+robertobuccina@users.noreply.github.com>
2021-04-08 23:21:22 +0200

    end

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 
 
-var kmPercorrenza = prompt("Km che si desidera percorrere");
-var etàPasseggero = prompt("età passeggero");
+var kmPercorrenza = parseInt(prompt("Km che si desidera percorrere"));
+var etaPasseggero =parseInt(prompt("età passeggero"));
+var prezzoBiglietto = (kmPercorrenza * 0.21)
 
-console.log(kmPercorrenza, etàPasseggero)
 
-if(kmPercorrenza ==="" || etàPasseggero === ""){
-    alert("dati mancanti e/o incompleti")
+if (Number.isNaN(kmPercorrenza) || Number.isNaN(prezzoBiglietto)){
+    alert ("dati mancanti o incompleti")
 }
+
+if (etaPasseggero >= 65) {
+    console.log (prezzoBiglietto - (prezzoBiglietto * 0.4));
+}
+
+if (etaPasseggero <= 18) {
+    console.log (prezzoBiglietto - (prezzoBiglietto * 0.2));
+}
+
+if (etaPasseggero > 18 || etaPasseggero > 65) {
+    console.log (prezzoBiglietto)
+}
+
+document.getElementById("risultato").innerHTML = "Euro" + " " + prezzoBiglietto;
+
+document.getElementById("passeggero").innerHTML = "Età Passeggero" + ":" + etaPasseggero;
+
+document.getElementById("km-tratta").innerHTML = "Km tratta" + ":" + kmPercorrenza;
+
+
+
+
+
+
+
+
