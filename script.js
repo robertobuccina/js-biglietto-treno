0e0216a6a46d7e00a05647ef44eea8af1e34ad67
robertobuccina <77169589+robertobuccina@users.noreply.github.com>
2021-04-08 15:38:07 +0200

    1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var kmPercorrenza = prompt("Km che si desidera percorrere");
+var etàPasseggero = prompt("età passeggero");
+
+console.log(kmPercorrenza, etàPasseggero)
+
+if(kmPercorrenza ==="" || etàPasseggero === ""){
+    alert("dati mancanti e/o incompleti")
+}
